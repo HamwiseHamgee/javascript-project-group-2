a9d0eb5fab80fd1d394751227f5c5d9e6537d816
christian zimmerman <czimmerman202@gmail.com>
2021-09-16 18:12:48 -0700

    javascript part

---
+++ posScript.js
@@ -0,0 +1,52 @@
+const products = [{
+        class: "gas",
+        img: "img/grom1.jpg",
+        name: "Honda Grom",
+        price: 2000,
+        description: "small bike, goes up to 60mph.",
+        id: "grom",
+    }
+
+]
+const shoppingCart = [];
+
+
+const createProduct = function(product) {
+    let productDiv = document.createElement('div')
+        //Creating div for the product
+    productDiv.setAttribute("id", product.id)
+        //Setting Id from product array
+        //productdiv innerHTML passing array info
+    productDiv.innerHTML = ` 
+    <a href="#"><img src="${product.img}"width="350" height="300"></a>
+    <h3>${product.name}</h3>
+     <p>Price: ${product.price}</p>
+     <p>${product.description}</p>`
+        //creating the Add to Cart btn
+    let btn = document.createElement('button');
+    //new button
+    btn.innerHTML = "Add to Cart";
+    //Text to btn
+    btn.classList.add('cartbtn')
+        //Attaching our add to cart class to the btn
+    productDiv.append(btn);
+    //appending the btn to the new div
+    document.querySelector('.' + product.class).append(productDiv);
+    //selecting the product class that matches users choice and adding it to the div
+    btn.addEventListener("click", event => {
+        shoppingCart.push(product);
+        console.log(shoppingCart);
+    })
+
+}
+for (product of products) {
+    // <div id='grom'>
+    //             <a href="#"><img src="img/grom1.jpg" alt="bike" width="350" height="300"></a>
+    //             <h3>Honda Grom</h3>
+    //             <p>Price: 2,000</p>
+    //             <p>small bike, goes up to 60mph.</p>
+    //             <button class="cartbtn">Add to cart</button>
+    //         </div>
+    createProduct(product);
+
+}
